cec816510a8437177a717ad937deae1f800705dc
Megdelawit365 <megdelawitaynalem365@gmail.com>
2026-09-07 20:39:20 +0300

    used the appointment routes

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,17 @@
 import express from "express"
 const app = express()
 app.use(express.json())
-
+import appointmentRoutes from "./routes/appointment.routes.js"
 const startTime = Date.now()
 
-app.get("/health", (req, res) => {
+app.get("/api/health", (req, res) => {
     const currentTime = Date.now()
     return res.json({
         timestamp: new Date().toISOString(),
         uptime: (currentTime - startTime) / 1000
     })
 })
+
+app.use("/api/appointments", appointmentRoutes)
 
 export default app
