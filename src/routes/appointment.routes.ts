b2396f82e6894cbf432e079352fdd5da4cb6dec1
Megdelawit365 <megdelawitaynalem365@gmail.com>
2026-09-07 20:38:55 +0300

    removed 'appointment' from the routes

--- src/routes/appointment.routes.ts
+++ src/routes/appointment.routes.ts
@@ -1,13 +1,13 @@
 import { Router } from "express"
 import { validateRequest } from "../middlewares/validate.middleware.js"
 import { createAppointmentSchema, updateAppointmentSchema, } from "../schemas/appointment.schema.js"
-import { getAppointments, getAppointmentById, createAppointment, updateAppointment, deleteAppointment, } from "../controllers/appointment.controller.js"
+import { getAppointments, getAppointmentById, createAppointment, updateAppointment, deleteAppointment } from "../controllers/appointment.controller.js"
 
 const router = Router()
-router.get("/appointments", getAppointments)
-router.get("/appointments/:id", getAppointmentById)
-router.post("/appointments", validateRequest(createAppointmentSchema), createAppointment)
-router.patch("/appointments/:id", validateRequest(updateAppointmentSchema), updateAppointment)
-router.delete("/appointments/:id", deleteAppointment)
+router.get("/", getAppointments)
+router.get("/:id", getAppointmentById)
+router.post("", validateRequest(createAppointmentSchema), createAppointment)
+router.patch("/:id", validateRequest(updateAppointmentSchema), updateAppointment)
+router.delete("/:id", deleteAppointment)
 
 export default router 
