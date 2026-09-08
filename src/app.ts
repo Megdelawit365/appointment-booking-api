1b364a9a114a27477ea1a2fa00bd0935694aaa70
Megdelawit365 <megdelawitaynalem365@gmail.com>
2026-09-08 13:34:01 +0300

    registered the middlewares

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ import express from "express"
 const app = express()
 app.use(express.json())
 import appointmentRoutes from "./routes/appointment.routes.js"
+import { notFound } from "./middlewares/notFound.js"
+import { errorHandler } from "./middlewares/error.middleware.js"
+
 const startTime = Date.now()
 
 app.get("/api/health", (req, res) => {
@@ -13,5 +16,6 @@ app.get("/api/health", (req, res) => {
 })
 
 app.use("/api/appointments", appointmentRoutes)
-
+app.use(notFound)
+app.use(errorHandler)
 export default app
