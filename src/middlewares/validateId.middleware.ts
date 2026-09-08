1ba57c5c5b02b68daa5f5b80ed28ad45efb64ca7
Megdelawit365 <megdelawitaynalem365@gmail.com>
2026-09-08 12:38:02 +0300

    added validateId middleware

---
+++ src/middlewares/validateId.middleware.ts
@@ -0,0 +1,11 @@
+import { NextFunction, Request, Response } from "express";
+
+const validateId = async (req: Request<{ id: string }>, res: Response, next: NextFunction) => {
+    const id = Number(req.params.id)
+
+    if (isNaN(id) || !Number.isInteger(id) || id <= 0) {
+        return res.status(400).json({ message: "Invalid ID." })
+    }
+
+    next()
+}
