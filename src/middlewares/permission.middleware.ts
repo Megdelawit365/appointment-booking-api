58719a15c869d29d897ed1886a13a2e0bdbcd668
Megdelawit365 <megdelawitaynalem365@gmail.com>
2026-09-10 17:00:24 +0300

    added permission middleware

---
+++ src/middlewares/permission.middleware.ts
@@ -0,0 +1,15 @@
+import { Request, Response, NextFunction } from "express"
+
+export const requirePermission = (permission: string) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        if (!req.user) {
+            return res.status(401).json({ message: "Authentication required" })
+        }
+
+        if (!req.user.permissions.includes(permission)) {
+            return res.status(403).json({ message: "Forbidden" })
+        }
+
+        next()
+    }
+} 
