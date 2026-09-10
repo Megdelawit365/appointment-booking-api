4e8652b2ab14f04c444a4b540a79d319b8c5e41e
Megdelawit365 <megdelawitaynalem365@gmail.com>
2026-09-10 14:30:54 +0300

    added more keys to env schema

--- src/config/env.ts
+++ src/config/env.ts
@@ -5,9 +5,11 @@ const envSchema = z.object({
     NODE_ENV: z.enum(["development", "test", "production"]).default("development"),
     PORT: z.coerce.number().int().positive().default(3000),
     JWT_ACCESS_SECRET: z.string().min(32),
+    JWT_REFRESH_SECRET: z.string().min(32),
     GOOGLE_CLIENT_ID: z.string().min(1),
     GOOGLE_CLIENT_SECRET: z.string().min(1),
     GOOGLE_CALLBACK_URL: z.url(),
+    DATABASE_URL: z.url(),
 })
 
 export const env = envSchema.parse(process.env) 
